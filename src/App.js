dba40282f7eaf25c21ef38f2b7de847229fa8936
Michał Janik <mjanik167@gmail.com>
2025-10-20 22:53:08 +0200

    ripple no colision

--- src/App.js
+++ src/App.js
@@ -2,19 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useEffect, useLayoutEffect } from 'react';
 
+
+class Ripple {
+  constructor(x, y, ctx) {
+    this.x = x;
+    this.y = y;
+    this.ctx = ctx;
+    this.radius = 0;
+    this.maxRadius = 10000;
+    this.lineWidth = 5;
+    this.alpha = 1.0;
+  }
+
+  draw() {
+    this.ctx.moveTo(this.x, this.y);
+    this.ctx.beginPath();
+    this.ctx.arc(this.x, this.y, this.radius++, 0, 2 * Math.PI);
+    this.ctx.stroke();
+  }
+}
+
 function App() {
   const canvasRef = React.useRef(null);
+  const ripples = React.useRef([]);
 
   const [windowWidth, setWindowWidth] = React.useState(window.innerWidth);
   const [windowHeight, setWindowHeight] = React.useState(window.innerHeight);
 
-  const handleClick = (e) => {
 
-    canvasRef.current.moveTo(e.pageX, e.pageY);
-    canvasRef.current.beginPath();
-    canvasRef.current.arc(e.pageX, e.pageY, 20, 0, 2 * Math.PI);
-    canvasRef.current.stroke();
-    console.log("line");
+  const handleClick = (e) => {
+    ripples.current.push(new Ripple(e.pageX, e.pageY, canvasRef.current));
   };
 
   const handleResize = () => {
@@ -23,13 +40,28 @@ function App() {
   }
 
 
+  const draw = () => {
+    canvasRef.current.clearRect(0, 0, windowWidth, windowHeight);
+
+    ripples.current.forEach((ripple, index) => {
+      ripple.draw();
+      if (ripple.radius > ripple.maxRadius) {
+        ripples.current.splice(index, 1);
+      }
+    })
+
+    requestAnimationFrame(draw);
+  };
 
   useLayoutEffect(() => {
     let canvas = document.querySelector('.App');
     canvasRef.current = canvas.getContext('2d');
-    console.log(canvasRef.current);
+    canvasRef.current.strokeStyle = '#c4c4c4ff';
+    canvasRef.current.lineWidth = 10;
     window.addEventListener('resize', handleResize);
-    console.log("resize listener added");
+
+    requestAnimationFrame(draw);
+
     return () => {
       window.removeEventListener('resize', handleResize);
     }
