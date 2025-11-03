80300d2c44d7a92e04aa34fa8a4b247fcd15bdf2
Michał Janik <mjanik167@gmail.com>
2025-11-03 21:17:34 +0100

    raycast

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState, useLayoutEffect, useRef } from 'react';
+
+const pi = Math.PI;
+const mapX = 8, mapY = 8, tileSize = 64;
+
+const map = [
+  1, 1, 1, 1, 1, 1, 1, 1,
+  1, 0, 0, 1, 0, 0, 0, 1,
+  1, 0, 0, 1, 0, 0, 0, 1,
+  1, 0, 0, 1, 0, 0, 0, 1,
+  1, 0, 0, 1, 1, 0, 0, 1,
+  1, 0, 0, 0, 0, 0, 0, 1,
+  1, 0, 0, 0, 0, 1, 0, 1,
+  1, 1, 1, 1, 1, 1, 1, 1,
+
+]
 
 
 
@@ -31,39 +45,169 @@ class Player {
     this.y = y;
     this.dx = 0;
     this.dy = 0;
-    this.a = 0
+    this.right = 0;
+    this.left = 0;
+    this.a = 0;
     this.ctx = ctx;
   }
 
   keyDown(e) {
-    console.log(this);
+    console.log(e.key);
 
     switch (e.key) {
       case 'w':
-        this.y -= 10;
+        this.y += this.dy * 10;
+        this.x += this.dx * 10;
         break;
       case 'a':
-        this.x -= 10;
+        this.y -= this.left * 10;
+        this.x -= this.right * 10;
         break;
       case 's':
-        this.y += 10;
+        this.y -= this.dy * 10;
+        this.x -= this.dx * 10;
         break;
       case 'd':
-        this.x += 10;
+        this.y += this.left * 10;
+        this.x += this.right * 10;
         break;
+      case 'ArrowUp':
 
+        break;
+      case 'ArrowDown':
+
+        break;
+      case 'ArrowLeft':
+        this.a -= 0.1;
+        if (this.a < 0) this.a = 2 * pi;
+        this.dx = Math.cos(this.a);
+        this.dy = Math.sin(this.a);
+        this.right = Math.cos(this.a + pi / 2);
+        this.left = Math.sin(this.a + pi / 2);
+        break;
+      case 'ArrowRight':
+        this.a += 0.1;
+        if (this.a > 2 * pi) this.a = 0;
+        this.dx = Math.cos(this.a);
+        this.dy = Math.sin(this.a);
+        this.right = Math.cos(this.a + pi / 2);
+        this.left = Math.sin(this.a + pi / 2);
+        break;
       default:
         break;
     }
   }
 
   draw() {
+
+    //direction indicator
+    // this.ctx.strokeStyle = '#0000ffff';
+    // this.ctx.beginPath();
+    // this.ctx.moveTo(this.x, this.y);
+    // this.ctx.lineTo(this.x + this.dx * 100, this.y + this.dy * 100);
+    // this.ctx.stroke();
+
+
     this.ctx.fillStyle = '#ff0000ff';
+    //player body
     this.ctx.beginPath();
-    this.ctx.arc(this.x, this.y, 10, 0, 2 * Math.PI);
+    this.ctx.arc(this.x, this.y, 5, 0, 2 * Math.PI);
     this.ctx.fill();
+    this.rayCast();
+
   }
+
+  rayCast() {
+    let r, rx, ry, ra, xo, yo, dof;
+    let mp, mx, my
+    ra = this.a
+
+    for (r = 0; r < 1; r++) {
+
+      dof = 0
+      let aTan = -1 / Math.tan(ra)
+      if (ra > pi) {
+        ry = Math.floor(this.y / tileSize) * tileSize - 0.0001
+        rx = (this.y - ry) * aTan + this.x
+        console.log(ry, rx);
+        yo = -tileSize;
+        xo = -yo * aTan;
+      }
+      if (ra < pi) {
+        ry = Math.floor(this.y / tileSize) * tileSize + tileSize
+        rx = (this.y - ry) * aTan + this.x
+        yo = tileSize;
+        xo = -yo * aTan;
+      }
+      if (ra == 0 || ra == pi) {
+        rx = this.x
+        ry = this.y
+        dof = 8
+      }
+      while (dof < 4) {
+        mx = Math.floor(rx / tileSize)
+        my = Math.floor(ry / tileSize)
+        mp = my * mapX + mx;
+
+        if (mp < mapX * mapY && map[mp] == 1) {
+          dof = 8
+        } else {
+          rx += xo
+          ry += yo
+          dof += 1
+        }
+      }
+      console.log(rx, ry)
+      this.ctx.strokeStyle = '#4adb37ff';
+      this.ctx.beginPath();
+      this.ctx.moveTo(this.x, this.y);
+      this.ctx.lineTo(rx, ry);
+      this.ctx.stroke();
+
+
+      dof = 0
+      let nTan = -Math.tan(ra)
+      if (ra > pi / 2 && ra < pi * 3 / 2) {
+        rx = Math.floor(this.x / tileSize) * tileSize - 0.0001
+        ry = (this.x - rx) * nTan + this.y
+        xo = -tileSize;
+        yo = -xo * nTan;
+      }
+      if (ra < pi / 2 || ra > pi * 3 / 2) {
+        rx = Math.floor(this.x / tileSize) * tileSize + tileSize
+        ry = (this.x - rx) * nTan + this.y
+        xo = tileSize;
+        yo = -xo * nTan;
+      }
+      if (ra == 0 || ra == pi) {
+        rx = this.x
+        ry = this.y
+        dof = 8
+      }
+      while (dof < 4) {
+        mx = Math.floor(rx / tileSize)
+        my = Math.floor(ry / tileSize)
+        mp = my * mapX + mx;
+
+        if (mp < mapX * mapY && map[mp] == 1) {
+          dof = 8
+        } else {
+          rx += xo
+          ry += yo
+          dof += 1
+        }
+      }
+      this.ctx.strokeStyle = '#db3797ff';
+      this.ctx.beginPath();
+      this.ctx.moveTo(this.x, this.y);
+      this.ctx.lineTo(rx, ry);
+      this.ctx.stroke();
+    }
+  }
+
 }
+
+
 
 
 function App() {
@@ -106,19 +250,6 @@ function App() {
 
 
 
-  let mapX = 8, mapY = 8, tileSize = 64;
-  let map = [
-    1, 1, 1, 1, 1, 1, 1, 1,
-    1, 0, 0, 1, 0, 0, 0, 1,
-    1, 0, 0, 1, 0, 0, 0, 1,
-    1, 0, 0, 1, 0, 0, 0, 1,
-    1, 0, 0, 1, 1, 0, 0, 1,
-    1, 0, 0, 0, 0, 0, 0, 1,
-    1, 0, 0, 0, 0, 1, 0, 1,
-    1, 1, 1, 1, 1, 1, 1, 1,
-
-  ]
-
 
 
   const drawMap = () => {
@@ -141,7 +272,6 @@ function App() {
     drawMap();
 
     player.current.draw();
-    console.log(player.current.y);
 
 
     ripples.current.forEach((ripple, index) => {
